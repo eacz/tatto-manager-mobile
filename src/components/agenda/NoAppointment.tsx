97bc04382ee449d7fde3ccf18976e8652ff2743c
Esteban Canteros <estebanacanteros@gmail.com>
2023-01-20 14:19:18 -0300

    implement no appoinment screen

--- src/components/agenda/NoAppointment.tsx
+++ src/components/agenda/NoAppointment.tsx
@@ -1,18 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { useContext } from 'react';
-import { ThemeContext } from '../../context/themeContext/ThemeContext';
+import { StyleSheet, Text, View } from 'react-native'
+import { useContext } from 'react'
+import { ThemeContext } from '../../context/themeContext/ThemeContext'
 
 export const NoAppointment = () => {
-  const {theme: { colors }} = useContext(ThemeContext)
-  
+  const {
+    theme: { colors },
+  } = useContext(ThemeContext)
+
   return (
-    <View style={{...styles.container, backgroundColor: colors.background}}>
-      <Text style={{...styles.text, color: colors.text}}>No hay turnos hoy</Text>
+    <View style={{ ...styles.container, backgroundColor: colors.background }}>
+      <Text style={{ ...styles.text, color: colors.text }}>No hay turnos hoy</Text>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
-  container: {flex: 1, alignItems: 'center', justifyContent: 'flex-start',},
-  text: { fontSize: 20, marginTop: 20},
-});
+  container: { display: 'flex', alignItems: 'center', justifyContent: 'center', height: '50%' },
+  text: { fontSize: 20, marginTop: 20, fontWeight: 'bold' },
+})
--- src/components/appointment/Appointments.tsx
+++ src/components/appointment/Appointments.tsx
@@ -1,11 +1,14 @@
 import { View, StyleSheet } from 'react-native'
 import { useAppSelector } from '../../hooks/redux'
 import { AppointmentSummary } from '.'
+import { NoAppointment } from '../agenda'
 const Appointments = () => {
   const { currentAppointments } = useAppSelector((state) => state.appointment)
+  console.log({ currentAppointments })
 
   return (
     <View style={styles.container}>
+      {currentAppointments.length === 0 && <NoAppointment />}
       {currentAppointments.map((appoinment) => (
         <AppointmentSummary appointment={appoinment} key={appoinment._id} />
       ))}
